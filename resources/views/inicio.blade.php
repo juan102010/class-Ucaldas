02ed5d3754fe01fd5f514f6efa2bb48886831ed6
Alejo1201 <equinterogiraldo12@gmail.com>
2023-11-11 21:48:38 -0500

    changesA

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,61 +9,36 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <style>
-        /* Estilos adicionales para la interfaz de chat */
-        body {
-            background-color: #f8f9fa;
-        }
-
-        #chat-container {
-            max-width: 1250px;
-            margin: 10px;
-            border: 1px solid #ddd;
-            border-radius: 8px;
-            overflow: hidden;
-        }
-
-        #chat-messages {
-            height: 350px;
-            overflow-y: scroll;
-            padding: 10px;
-            background-color: #fff;
-        }
-
-        #user-input {
-            padding: 10px;
-            background-color: #fff;
-            border-top: 1px solid #ddd;
-        }
-    </style>
+        <link rel="stylesheet" href="resources/css/chat.css">
 </head>
 <body>
     
 <!-- Contenedor principal del chat -->
-<div class="container mt-4" id="chat-container">
+<div class="container mt-5">
     <!-- Barra lateral izquierda (menú) -->
-    <div class="d-none d-md-block" style="float: left; width: 20%; background-color: #343a40; color: #fff; height: 400px; padding: 20px;">
-        <h5>Menú</h5>
+    <div class="d-none d-md-block bg-dark text-white p-4" style="float: left; width: 20%; height: calc(100vh - 90px);">
+        <h5>Asistente Virtual</h5>
         <!-- Agrega aquí tus elementos de menú -->
-        <ul>
-                        
+        <ul class="list-unstyled">
+            <!-- Elementos de menú -->
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Materias</a></li>
+            <li><a href="#">Calificaciones</a></li>
         </ul>
     </div>
 
     <!-- Zona de chat -->
-    <div style="float: right; width: 80%;">
+    <div class="p-4" style="float: right; width: 80%; height: 570px; overflow-y: auto; border: 1px solid #ccc; border-radius: 8px;">
         <!-- Historial de mensajes -->
-        <div id="chat-messages">
-        
+        <div id="result" class="bg-light p-3 rounded mb-3 container mt-4" style="max-height: 70%; overflow-y: auto;">
+            <!-- Los mensajes se agregarán dinámicamente aquí -->
         </div>
 
         <!-- Barra de entrada de usuario -->
-        <div id="user-input">
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Escribe un mensaje...">
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button" onClick="createChat()">Enviar</button>
-                </div>
+        <div class="input-group mt-auto">
+            <input type="text" class="form-control" placeholder="Escribe un mensaje..." id="user-message">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="button" onClick="createChat()">Enviar</button>
             </div>
         </div>
     </div>
@@ -98,7 +73,9 @@
                 headers: headers,
                 success: function(response) {
                     console.log('Solicitud exitosa:', response);
-                    // Puedes manejar la respuesta del servidor aquí
+                    
+                    // Se utiliza la función para mostrar la información
+                    mostrarInformacion(response);
                 },
                 error: function(error) {
                     console.error('Error en la solicitud:', error);
@@ -106,6 +83,35 @@
                 }
             });
     }
+
+    function mostrarInformacion(data) {
+    // Supongamos que tienes un elemento con el ID 'resultado' en tu HTML
+    var resultadoElement = document.getElementById('result');
+
+    // Limpia el contenido previo del div
+    resultadoElement.innerHTML = '';
+
+    // Verifica si la respuesta tiene la propiedad 'Result' y 'Data'
+    if (data.Result && data.Result.Data && data.Result.Data.AnswerResult) {
+        // Obtiene la lista de preguntas
+        var preguntas = data.Result.Data.AnswerResult.Questions;
+
+        // Itera sobre cada pregunta y muestra el contenido de los mensajes
+        preguntas.forEach(function (pregunta) {
+            var mensajes = pregunta.Messages;
+
+            mensajes.forEach(function (mensaje) {
+                // Crea un nuevo elemento de párrafo para mostrar cada mensaje
+                var mensajeElement = document.createElement('p');
+                mensajeElement.textContent = mensaje.Content;
+            // Agrega el nuevo elemento al resultadoElement
+            resultadoElement.appendChild(mensajeElement);
+            });
+        });
+    } else {
+        resultadoElement.innerHTML = 'La estructura de la respuesta no es la esperada.';
+    }
+}
 </script>
 
 </html>
